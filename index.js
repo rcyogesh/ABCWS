4ca7788cbd288463ae07ea9cc9929edb14a00ddb
rcyogesh <rcyogesh@pradip.com>
2017-10-17 22:47:43 -0400

    File logging

--- index.js
+++ index.js
@@ -2,38 +2,46 @@ var http = require('http');
 const { Pool } = require('pg');
 const url = require('url');
 const util = require('util');
+const fs = require('fs');
+
+var pg = new Pool({      
+        user: 'rcyogesh@rcpgserver.postgres.database.azure.com',
+        host: 'rcpgserver.postgres.database.azure.com',
+        database: 'Test',
+        password: 'RC4yogesh',
+        port: 5432,
+    });
 
 var server = http.createServer(function(request, response) {
-
-    var pg = new Pool({      
-         user: 'rcyogesh@rcpgserver.postgres.database.azure.com',
-         host: 'rcpgserver.postgres.database.azure.com',
-         database: 'Test',
-         password: 'RC4yogesh',
-         port: 5432,
-        //  sslmode: 'require'
-     });
     const parsedURL = url.parse(request.url, true);
-    //response.write(request.url);
+    
     response.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin":"http://localhost:4200"});
-    pg.connect(function(err, client){
-        if(err) {
-            response.end(err.message);
-        }else  {
-            const res = client.query(util.format("SELECT * FROM public.\"Words\" where \"Words\" LIKE '%s%%'", parsedURL.query.letter), 
+
+    var letter = parsedURL.query.letter;
+    console.log(letter);
+    if(letter == "" || letter == undefined){
+        response.end("Please specify a letter");
+    }
+    else {
+        pg.query(util.format("SELECT * FROM public.\"Words\" where \"Words\" LIKE '%s%%'", parsedURL.query.letter),
             function(qErr, qRes) {
                 if(qErr) {
                     response.end(qErr.message);
                 }
                 else {
-                    response.write(JSON.stringify(qRes.rows.map(element=>element.Words)));
+                    var json = JSON.stringify(qRes.rows.map(element=>element.Words));
+                    response.write(json);
+                    fs.open("fileBlob.txt", "a", function(err, fd){
+                        if(!err) {
+                            fs.write(fd, json + "\r\n", function(err, written, str) {
+                                fs.close(fd);
+                            });
+                        }
+                    });
                     response.end();
                 }
             });
-        }
-    });
-
-    //response.end("hello azure")
+    }
 });
 
 var port = process.env.PORT || 1337;
